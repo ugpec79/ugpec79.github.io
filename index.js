7a5a4cf9bc7068b135c35eabb3023d05b96e2dfa
ugpec79 <113056503+ugpec79@users.noreply.github.com>
2022-11-06 20:33:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 var options={
-    strings:["Student","Perfectionist"],
+    strings:["Student","PECobian", "Coder","Web Developer","Tech Enthusiast"],
     typeSpeed:40,
     backSpeed:30,
     loop:true,
